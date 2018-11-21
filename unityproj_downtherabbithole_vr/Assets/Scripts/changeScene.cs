7015ab4efb860db752a7d6da427118089efb0b91
Anran1015 <anranzhou1015@gmail.com>
2018-11-21 16:30:28 -0500

    Scene Transition
    changeScene.cs is for manully switch between scenes. LoadLevelAfterTime.cs is to set a timer and switch scene.

---
+++ unityproj_downtherabbithole_vr/Assets/Scripts/changeScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Alpha1)){
+			Invoke( "ChangeLevel", 3.0f );
+			Application.LoadLevel("scene1");
+		}else if(Input.GetKeyDown(KeyCode.Alpha2)){
+			Invoke( "ChangeLevel", 3.0f );
+			Application.LoadLevel("scene2");
+		}else if(Input.GetKeyDown(KeyCode.Alpha3)){
+			Invoke( "ChangeLevel", 3.0f );
+			Application.LoadLevel("scene3");
+		}else if(Input.GetKeyDown(KeyCode.Alpha4)){
+			Invoke( "ChangeLevel", 3.0f );
+			Application.LoadLevel("scene4");
+		}
+	}
+}
+
+
